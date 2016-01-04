3e4f55fa46aee5aa417e8a974a2c95150e089ba4
xeodou <xeodou@gmail.com>
2016-01-05 01:13:52 +0900

    Change webpack log level.

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ new WebpackDevServer(webpack(config), {
   contentBase: path.join(__dirname, '.'),
   publicPath: config.output.publicPath,
   historyApiFallback: true,
-  hot: true
+  hot: true,
+  stats: {
+    chunks: false,
+    chunkModules: false
+  }
 }).listen(4000, 'localhost', function (err) {
   if (err) console.log(err)
   console.log('Listening at localhost:4000')
